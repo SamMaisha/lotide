ec7a4208e09c96a21a9b91f70d68e0109afd5444
Sam Maisha <sam.maisha99@gmail.com>
2022-11-23 17:54:03 +0000

    implemented findKeyByValue function which finds a key in an object based on the value provided

---
+++ findKeyByValue.js
@@ -0,0 +1,49 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// findKeyByValue FUNCTION IMPLEMENTATION
+
+//object; input object provided
+//value; value that will be used to find key within the object
+
+const findKeyByValue = function (object, value) {
+  for (const key in object) {
+    let valueToCompare = object[key];
+    if (valueToCompare === value) {
+      return key;
+    }
+  }
+};
+
+
+// TEST CODE
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+const treats = {
+  cake: "carrot cake",
+  chocolateBar: "kit-kat",
+  pie: "apple pie",
+  candy: "sour patch kids"
+};
+
+assertEqual(findKeyByValue(treats, "kit-kat"), "chocolateBar");
+assertEqual(findKeyByValue(treats, "sour patch kids"), "candy");
+assertEqual(findKeyByValue(treats, "pop tarts"), undefined);
+
+
+
+
+
