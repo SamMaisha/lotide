79e632ac4229bb2a9fe2d5f1ad400a057a45e6cc
Sam Maisha <sam.maisha99@gmail.com>
2022-11-23 15:57:45 +0000

    implemented countLetter function to count the number of times a letter occurs in a sentence and save it into an object

---
+++ countLetters.js
@@ -0,0 +1,43 @@
+// ASSERT EQUAL FUNCTION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// countLetters FUNCTION IMPLEMENTATION
+
+// countSentence; a string from which we will count of each letter in the string
+
+const countLetters = function (countSentence) {
+  let countLettersInSentence = {};
+  for (letters of countSentence) {
+    if (letters !== " ") {
+      if (countLettersInSentence[letters]) {
+        countLettersInSentence[letters] += 1;
+      } else {
+        countLettersInSentence[letters] = 1;
+      }
+    }
+  }
+  return countLettersInSentence;
+}
+
+// TEST CODE 
+
+result1 = countLetters('a bb ccc dddd eeeee ffffff');
+assertEqual(result1["a"], 1);
+assertEqual(result1["b"], 2);
+assertEqual(result1["c"], 3);
+assertEqual(result1["d"], 4);
+assertEqual(result1["e"], 5);
+assertEqual(result1["f"], 6);
+
+
+
+
+
+
