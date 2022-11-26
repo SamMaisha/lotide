fc2fe11f2d3ae8d04393cc6d2918fa540e763441
Sam Maisha <sam.maisha99@gmail.com>
2022-11-26 10:56:30 +0000

    implemented findKey function that finds a key based on a callback function

---
+++ findKey.js
@@ -0,0 +1,47 @@
+// findKey FUNCTION IMPLEMENTATION; scans object and returns the first key for which the callback returns a truthy value
+
+const findKey = function (object, callback) {
+  let result = "";
+  for (let key in object) {
+    if (callback(object[key])) {
+      result = key;
+      return result;
+    }
+  }
+};
+
+// TESTING
+
+// assertEqual FUNCTION
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// TEST
+
+result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2);
+
+assertEqual(result1, "noma");
+
+result2 = findKey({
+  Sam: 2,
+  Cody: 3,
+  Billy: 5,
+  Jake: 87
+}, x => x === 5);
+
+assertEqual(result2, "Billy");
+
+
+
