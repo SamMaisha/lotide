92ab52eb46aa1a08536bdc3c9c1d374dce4438f2
Sam Maisha <sam.maisha99@gmail.com>
2022-12-06 04:49:22 +0000

    refactored tail.js and added tailTest.js to Test folder

--- tail.js
+++ tail.js
@@ -1,21 +1,9 @@
-// ASSERT EQUAL FUNCTION TO POST PASS OR FAIL MESSAGE
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢 Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 // TAIL FUNCTION IMPLEMENTATION
-const tail = function (array) {
+const tail = function(array) {
   return array.slice(1);
 };
 
-// TEST
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+module.exports = tail;
+
 
 
